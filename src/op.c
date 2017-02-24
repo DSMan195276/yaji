84555c67b33adc7afd16b9ce9b6d09f6b58487bb
Matthew Kilgore <mattkilgore12@gmail.com>
2017-02-24 05:27:32 -0500

    Revert "Remove lvalue_scope_depth and rvalue_scope_depth"
    This reverts commit 18a16756e29b877d132caeb2873daa36cf79a7ee.

--- src/op.c
+++ src/op.c
@@ -24,6 +24,7 @@ static struct jaz_ast_entry *jaz_op_begin(struct jaz_vm *vm, struct jaz_ast_entr
     }
 
     vm->lvalue_scope = scope;
+    vm->lvalue_scope_depth++;
 
     return jaz_ast_next_entry(entry);
 }
@@ -34,11 +35,12 @@ static struct jaz_ast_entry *jaz_op_end(struct jaz_vm *vm, struct jaz_ast_entry 
 
     vm->rvalue_scope = list_prev_entry(scope, scope_entry);
 
-    if (list_is_last(&vm->scope_list, &scope->scope_entry)) {
+    if (vm->rvalue_scope_depth > vm->lvalue_scope_depth) {
         list_del(&scope->scope_entry);
         jaz_vm_del_scope(scope);
     }
 
+    vm->rvalue_scope_depth--;
     return jaz_ast_next_entry(entry);
 }
 
@@ -55,6 +57,7 @@ static struct jaz_ast_entry *jaz_op_call(struct jaz_vm *vm, struct jaz_ast_entry
     }
 
     vm->rvalue_scope = scope;
+    vm->rvalue_scope_depth++;
 
     nent = jaz_ast_next_entry(entry);
 
@@ -66,12 +69,22 @@ static struct jaz_ast_entry *jaz_op_call(struct jaz_vm *vm, struct jaz_ast_entry
 
 static struct jaz_ast_entry *jaz_op_return(struct jaz_vm *vm, struct jaz_ast_entry *entry)
 {
+    struct jaz_ast_entry *ret;
     struct jaz_vm_scope *scope = vm->lvalue_scope;
 
     vm->lvalue_scope = list_prev_entry(scope, scope_entry);
+
+    if (vm->lvalue_scope_depth > vm->rvalue_scope_depth) {
+        list_del(&scope->scope_entry);
+        jaz_vm_del_scope(scope);
+    }
+
+    vm->lvalue_scope_depth--;
+
+    ret = (struct jaz_ast_entry *)*vm->stack_top;
     vm->stack_top--;
 
-    return (struct jaz_ast_entry *)*vm->stack_top;
+    return ret;
 }
 
 /*
